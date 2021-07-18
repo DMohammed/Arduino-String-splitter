61d0caf2e7a0f72bc7fa5dc70e80dd2467ba9efb
Mohammed <MD.2015.1@outlook.com>
2021-07-18 11:02:21 +0430

    Add files via upload

---
+++ Splitter/Splitter.ino
@@ -0,0 +1,32 @@
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Ready");//reza,ali,Mohammed,kasra
+                          //    4   8        17
+}
+
+void loop()
+{
+
+  if (Serial.available())
+  {
+    Serial.println(splitter(Serial.readStringUntil("\n"), ',', 1));// read data from Srtial and return Secend value
+  }
+}
+
+String splitter(String value, char splitChar, byte selection)
+{
+  value.trim();
+  byte position[5] = {0, 0, 0, 0, value.length()};//if need more split add to this array
+  byte j = 1;
+  for (byte i = 0; i < value.length(); i++)
+  {
+    if (value.charAt(i) == Slipt_char)
+    {
+      position[j] = i+1;
+      j++;
+    }
+  }
+  return value.substring(position[selection], position[selection + 1]-1);
+}
